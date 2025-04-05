0a3bb86ef8d50ce9735348adaabf7231991ed403
Victiniiiii <victinii494@gmail.com>
2025-04-06 00:54:10 +0300

    included lib.rs in main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    app_lib::run();
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_notification::init())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
